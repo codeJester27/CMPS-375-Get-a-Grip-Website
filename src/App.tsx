291c31692756072371722431ae8b3d7896143506
Jared Talbot <jared.talbot-2@selu.edu>
2024-04-20 21:02:02 -0500

    add links

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,52 @@ import { ControlMenu, ErrorMenu, StartMenu } from "./menus";
 
 function App() {
   return (
-    <WebSocketContext>
-      <Menus />
-    </WebSocketContext>
+    <>
+      <WebSocketContext>
+        <Menus />
+        <Footer />
+      </WebSocketContext>
+    </>
   );
 }
 
 function Menus() {
   const { serverAddress, status } = useWebSocketContext();
-  return serverAddress ? (
-    status === "connecting" ? (
-      <Loading />
-    ) : status === "error" ? (
-      <ErrorMenu message="Connection Failed" />
-    ) : status === "disconnected" ? (
-      <ErrorMenu message="Disconnected" />
-    ) : (
-      <ControlMenu />
-    )
-  ) : (
-    <StartMenu />
+  return (
+    <div className="menus">
+      {serverAddress ? (
+        status === "connecting" ? (
+          <Loading />
+        ) : status === "error" ? (
+          <ErrorMenu message="Connection Failed" />
+        ) : status === "disconnected" ? (
+          <ErrorMenu message="Disconnected" />
+        ) : (
+          <ControlMenu />
+        )
+      ) : (
+        <StartMenu />
+      )}
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="footer">
+      <a
+        href="https://github.com/codeJester27/CMPS-375-Get-a-Grip-Website"
+        target="_blank"
+      >
+        Github Repository
+      </a>
+      <a
+        href="https://codejester27.github.io/Get-a-Grip-Virtual-Hand/"
+        target="_blank"
+      >
+        Virtual Hand
+      </a>
+    </div>
   );
 }
 
